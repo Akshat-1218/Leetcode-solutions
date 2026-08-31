0dec9068ae34fa42c679caf9372dedcff0e3c576
Akshat Pandey <akshatpandey018@gmail.com>
2026-08-31 21:47:11 +0530

    Time: 4 ms (56%), Space: 52.7 MB (40.9%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,14 +1,43 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> l = new ArrayList<>();
-        int n = (nums.length/3)+1;
-        HashMap<Integer,Integer> hm = new HashMap<>();
+        int elem1 = 0;
+        int elem2 = 0;
+        int count1 = 0 ;
+        int count2 = 0;
+        int n = nums.length/3;
         for(int i:nums){
-            hm.put(i,hm.getOrDefault(i,0)+1);
-            if(hm.get(i) == n){
-                l.add(i);
+            if(count1 == 0 && elem2 != i){
+                elem1 = i;
+                count1++;
+            }
+            else if(count2 == 0 && elem1 != i){
+                elem2 = i;
+                count2++;
+            }
+            else if(elem1 == i){
+                count1++;
+            }
+            else if(elem2 == i){
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
             }
         }
+        count1 = 0;
+        count2 = 0;
+        for (int i : nums) {
+            if (i == elem1) {
+                count1++;
+            }
+            else if (i == elem2) {
+                count2++;
+            }
+        }
+        if(count1 > n) l.add(elem1);
+        if(count2 > n) l.add(elem2);
         return l;
     }
 }
